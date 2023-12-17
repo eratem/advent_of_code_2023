2b9a261ccd872a4b558f11f1edb040bb2f68fcce
René Mettler <rene.mettler92@gmail.com>
2023-12-17 16:53:24 +0100

    solve puzzle1 day3

--- day3.py
+++ day3.py
@@ -19,29 +19,26 @@ def _search_numbers_with_indexes(line: str) -> list[IndexedNumber]:
             if current_number == 0:
                 start_index = index
             current_number = current_number * 10 + int(character)
-        else:
-            if current_number == 0:
-                continue
-            else:
-                results.append(IndexedNumber(start_index, index, current_number))
-                current_number = 0
+        elif current_number != 0:
+            results.append(IndexedNumber(start_index - 1, index, current_number))
+            current_number = 0
     if current_number != 0:
-        results.append(IndexedNumber(start_index, len(line), current_number))
+        results.append(IndexedNumber(start_index - 1, len(line), current_number))
     return results
 
 
 def _find_numbers_with_neighbouring_symbols(
-    numbers: list[IndexedNumber], frame: list[str]
+    indexed_numbers: list[IndexedNumber], frame: list[str]
 ) -> list[int]:
     results = []
-    for number in numbers:
-        if number.start_index == 0:
+    for indexed_number in indexed_numbers:
+        if indexed_number.start_index <= 0:
             start_window = 0
         else:
-            start_window = number.start_index - 1
+            start_window = indexed_number.start_index
         matches_symbol = False
         for line in frame:
-            window = line[start_window : number.end_index + 1]
+            window = line[start_window : indexed_number.end_index + 1]
             for character in window:
                 if (not character.isdigit()) and (character != "."):
                     matches_symbol = True
@@ -49,41 +46,28 @@ def _find_numbers_with_neighbouring_symbols(
             if matches_symbol:
                 break
         if matches_symbol:
-            results.append(number.number)
+            results.append(indexed_number.number)
     return results
 
 
 def solve_puzzle1(data: list[str]) -> int:
+    data = [line.strip() for line in data]
     number_of_rows = len(data)
     matches = []
-    for index in range(number_of_rows):
+    for index, current_row in enumerate(data):
         if index == 0:
-            frame = ["".join(["." for _ in data[0]])] + data[index : index + 2]
+            frame = data[index : index + 2]
         elif index == number_of_rows - 1:
-            frame = data[index - 1 : number_of_rows] + ["".join(["." for _ in data[0]])]
+            frame = data[index - 1 : number_of_rows]
         else:
             frame = data[index - 1 : index + 2]
-        numbers = _search_numbers_with_indexes(data[index])
+        numbers = _search_numbers_with_indexes(current_row)
         matched_numbers = _find_numbers_with_neighbouring_symbols(numbers, frame)
         matches += matched_numbers
     return sum(matches)
 
 
 def main() -> None:
-    sample_puzzle = """
-467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755#
-..........
-.664#598..
-........10
-.........%
-""".splitlines()
     with open(Path("puzzle_day3.txt"), "r") as file:
         data = file.readlines()
 
